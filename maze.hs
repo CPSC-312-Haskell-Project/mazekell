293fb6c9da32bddd870c50eac2ce708a757112fb
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-10-12 20:49:42 -0700

    Generating random numbers and random cells

--- maze.hs
+++ maze.hs
@@ -1,7 +1,28 @@
 module Maze where
 
 import Data.HashSet
+import System.Random
+import System.IO.Unsafe (unsafePerformIO)
 
+-- wall representation in our maze. The left wall, right wall, upper wall and lower wall
+-- are represented as 'L', 'R', etc. respectively
 walls = ['L', 'R', 'U', 'D']
 
-createGrid n = [(r, c, w) | r <- [1..n], c <- [1..n], w <- walls]
+-- Easier to write function to convert Int -> Num
+num n = fromIntegral n
+
+-- Creates a grid list full of walls
+createGrid gridSize = [(num r, num c, w) | r <- [1..gridSize], c <- [1..gridSize], w <- walls]
+
+-- creates an empty HashSet
+-- More verbose than simply "empty"
+createEmptySet = empty
+
+-- Generates a random number of the for IO a
+randomNumberIO lowerBound upperBound = randomRIO (lowerBound, upperBound)
+
+-- Converts IO a -> a for easier operations on the random number generated
+randomNumber lowerBound upperBound = unsafePerformIO (randomNumberIO lowerBound upperBound)
+
+-- generates a random grid cell
+randomCell gridSize = (num (randomNumber 1 gridSize), num (randomNumber 1 gridSize)) 
