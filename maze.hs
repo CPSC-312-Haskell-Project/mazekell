7a08165cd6113d057109c23476c4e61337a259e9
Eishan Lawrence <eishanlawrence5@gmail.com>
2019-10-13 16:12:33 -0700

    Split the maze creation logic and user interaction logic into separate files

--- maze.hs
+++ maze.hs
@@ -1,86 +1,10 @@
 module Maze where
 
-import Data.Char
 import Data.HashSet
-import System.Environment
-import System.Exit
 import System.Random
 
--- ALL IO AND RANDOM NUMBER LOGIC IS HERE
-
-main :: IO()
-main = do
-   args <- getArgs
-   verifyArgsOrQuit args
-   seed <- getSeed args
-   showSeed seed
-   putStrLn "Please enter the grid size"
-   mazeSize <- getNum "\n Please enter a valid number:"
-   putStrLn "Now Creating a Maze"
-
--- Prompt until a valid number is read, and return it
-getNum :: String -> IO Int
-getNum prompt =
-  getFromStdin prompt getLine isNum read
-
--- This contains the logic common to getNum and getYN;
--- it repeatedly prompts until input matching some criteria
--- is given, transforms that input, and returns it
-getFromStdin :: String -> (IO a) -> (a -> Bool) -> (a -> b) -> IO b
-getFromStdin prompt inputF isOk transformOk = do
-  input <- inputF
-  if isOk input
-     then return $ transformOk input
-     else do
-       putStr prompt
-       getFromStdin prompt inputF isOk transformOk
-
--- create a random generator with the specified seed value
-getRandomGen :: Int -> StdGen
-getRandomGen seed = mkStdGen seed
-
--- If a seed is specified, use it; otherwise, pick a random one
-getSeed :: [String] -> IO Int
-getSeed [] = getRandomSeed
-getSeed (x:_) = return $ read x
-
--- Use the pre-seeded random generator to get a random seed
-getRandomSeed :: IO Int
-getRandomSeed = do
-  randomSrc <- getStdGen
-  return $ fst $ random $ randomSrc
-
--- To ouput seed to the user, so that the same maze can be generated again
-showSeed :: Int -> IO ()
-showSeed seed = putStrLn $ "The random seed is " ++ show seed
-
--- Argument verification code
-verifyArgsOrQuit :: [String] -> IO ()
-verifyArgsOrQuit args =
-  if verifyArgs args
-     then putStrLn "args ok!"
-     else exitWithBadArgs
-
-exitWithBadArgs :: IO ()
-exitWithBadArgs = do 
-  progName <- getProgName
-  putStrLn $ "Use: " ++  progName ++ " [optional random seed]"
-  exitWith $ ExitFailure 1
-
--- Legitimate arguments are none, or a string representing
--- a random seed.  Nothing else is accepted.
-verifyArgs :: [String] -> Bool
-verifyArgs [] = True
-verifyArgs (x:xs) = Prelude.null xs && isNum x
-
--- Verify that input is a number.  This approach was chosen as read raises an
--- exception if it can't parse its input
-isNum :: String -> Bool
-isnum [] = False 
-isNum (x:xs) = all isDigit xs && (x == '-' || isDigit x)
-
-
--- ALL MAZE LOGIC IS BELOW THIS
+-- createMaze :: Int -> StdGen -> Int
+createMaze randomGen = next randomGen
 
 -- creates an empty HashSet
 -- More verbose than simply "empty"
